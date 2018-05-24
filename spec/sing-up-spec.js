7165691528e7862c0fb7e98beb5d754920bef1d8
imkatrinaxx <kspanerio@gmail.com>
2018-05-24 18:31:39 +0800

    sign-up

--- spec/sing-up-spec.js
+++ spec/sing-up-spec.js
@@ -5,6 +5,8 @@ const faker = require('faker');
 
 var email = faker.internet.email()
 var password = faker.internet.password()
+var base_url = 'http://localhost:3001/';
+      
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
@@ -12,33 +14,16 @@ describe('route: /signup', function() {
   let config = { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } };
 
   beforeEach(function() {
-      this.root = 'http://localhost:3001/';
-      this.url = this.root + 'signup';
+      this.url = base_url+'signup'
   });
 
   it('should handle POST requests', async function() {
       const response = await axios.post(this.url, qs.stringify({'email':email,'password':password, 'confirmation':'password'}));
-      expect(response.status).toBeLessThan(400);
+      expect(response.status).toBe(200);
   });
 
   it('should sign up successfully', async function() {
     const response = await axios.post( this.url, qs.stringify({'email':email,'password':password, 'confirmation':password}));
-    expect(response.request.res.responseUrl).toBe(this.root+'profile');
-  });
-
-  it('should identify if email already in use', async function() {
-    const response = await axios.post(this.url, qs.stringify({'email':email,'password':'flames', 'confirmation':'flames'}));
-    expect(response.request.res.responseUrl).toBe(this.root);
-
-    /*const $ = cheerio.load(response.data);
-    assert.equal($('h3#signupMsg').text(), 'Email is already in use.');*/
-  });
-
-  it('should identify passwords do not match', async function() {
-    const response = await axios.post(this.url, qs.stringify({'email':email,'password':password, 'confirmation':'flames'}));
-    expect(response.request.res.responseUrl).toBe(this.root);
-
-   /* const $ = cheerio.load(response.data);
-    assert.equal($('h3#signupMsg').text(), 'Passwords do not match.');*/
+    expect(response.request.res.responseUrl).toBe(base_url+'profile');
   });
 });
