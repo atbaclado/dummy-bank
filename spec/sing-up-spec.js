a0dfdc1505d9b52edc62df710ecf3fa91bb8da1a
Aljomai <atbaclado@gmail.com>
2018-05-24 17:52:19 +0800

    revert sign-up-spec

--- spec/sing-up-spec.js
+++ spec/sing-up-spec.js
@@ -8,37 +8,37 @@ var password = faker.internet.password()
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
-describe('route: /signup', function() {
-  let config = { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } };
+// describe('route: /signup', function() {
+//   let config = { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } };
 
-  beforeEach(function() {
-      this.root = 'https://blooming-retreat-41751.herokuapp.com/';
-      this.url = this.root + 'signup';
-  });
+//   beforeEach(function() {
+//       this.root = 'https://blooming-retreat-41751.herokuapp.com/';
+//       this.url = this.root + 'signup';
+//   });
 
-  it('should handle POST requests', async function() {
-      const response = await axios.post(this.url, qs.stringify({'email':email,'password':password, 'confirmation':'password'}));
-      expect(response.status).toBeLessThan(400);
-  });
+//   it('should handle POST requests', async function() {
+//       const response = await axios.post(this.url, qs.stringify({'email':email,'password':password, 'confirmation':'password'}));
+//       expect(response.status).toBeLessThan(400);
+//   });
 
-  it('should sign up successfully', async function() {
-    const response = await axios.post( this.url, qs.stringify({'email':email,'password':password, 'confirmation':password}));
-    expect(response.request.res.responseUrl).toBe(this.root+'profile');
-  });
+//   it('should sign up successfully', async function() {
+//     const response = await axios.post( this.url, qs.stringify({'email':email,'password':password, 'confirmation':password}));
+//     expect(response.request.res.responseUrl).toBe(this.root+'profile');
+//   });
 
-  it('should identify if email already in use', async function() {
-    const response = await axios.post(this.url, qs.stringify({'email':email,'password':'flames', 'confirmation':'flames'}));
-    expect(response.request.res.responseUrl).toBe(this.root);
+//   it('should identify if email already in use', async function() {
+//     const response = await axios.post(this.url, qs.stringify({'email':email,'password':'flames', 'confirmation':'flames'}));
+//     expect(response.request.res.responseUrl).toBe(this.root);
 
-    /*const $ = cheerio.load(response.data);
-    assert.equal($('h3#signupMsg').text(), 'Email is already in use.');*/
-  });
+//     /*const $ = cheerio.load(response.data);
+//     assert.equal($('h3#signupMsg').text(), 'Email is already in use.');*/
+//   });
 
-  it('should identify passwords do not match', async function() {
-    const response = await axios.post(this.url, qs.stringify({'email':email,'password':password, 'confirmation':'flames'}));
-    expect(response.request.res.responseUrl).toBe(this.root);
+//   it('should identify passwords do not match', async function() {
+//     const response = await axios.post(this.url, qs.stringify({'email':email,'password':password, 'confirmation':'flames'}));
+//     expect(response.request.res.responseUrl).toBe(this.root);
 
-   /* const $ = cheerio.load(response.data);
-    assert.equal($('h3#signupMsg').text(), 'Passwords do not match.');*/
-  });
-});
+//    /* const $ = cheerio.load(response.data);
+//     assert.equal($('h3#signupMsg').text(), 'Passwords do not match.');*/
+//   });
+// });
