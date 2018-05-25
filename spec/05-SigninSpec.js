f04c8f352a55cec8c6f021213ec8d072779f8567
Aljomai <atbaclado@gmail.com>
2018-05-25 15:53:18 +0800

    update tdd tests to bdd

---
+++ spec/05-SigninSpec.js
@@ -0,0 +1,39 @@
+// define(function(require) {
+  const axios = require('axios');
+  const cheerio = require('cheerio');
+  const qs = require('qs');
+  const faker = require('faker');
+
+  axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+  describe('route: /signin', function() {
+    var email = faker.internet.email()
+    var password = faker.internet.password()
+
+  beforeEach(function() {
+      this.root = 'http://localhost:3000/';
+      this.url = this.root + 'signup';
+  });
+
+    it('should handle POST requests', async function() {
+        const response = await axios.post(this.url, qs.stringify({'email':email,'password':password, 'confirmation':'password'}));
+        expect(response.status).toBeLessThan(400);
+    });
+
+    it('should sign in successfully', async function() {
+      const response = await axios.post(this.url, qs.stringify({'email':'atbaclado@gmail.com','password':'flames'}));
+      // expect(response.request.res.responseUrl).toBe(this.root + 'profile');
+      expect(response.status).toBe(200);
+    });
+
+    it('should identify if email is incorrect', async function() {
+      const response = await axios.post(this.url, qs.stringify({'email':email,'password':'flames'}));
+      expect(response.request.res.responseUrl).toBe(this.root);
+    });
+
+    it('should identify if password is incorrect', async function() {
+      const response = await axios.post(this.url, qs.stringify({'email':'atbaclado@gmail.com','password':password, 'confirmation':'flames'}));
+      expect(response.request.res.responseUrl).toBe(this.root);
+    });
+  });
+// })
